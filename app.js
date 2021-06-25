511e33c308f3c638b9c2a2786d7590d7e6321d17
David Salguero <mada.salgui@gmail.com>
2021-06-24 23:32:46 -0600

    Port 3000 configuration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+app.set('port', process.env.PORT || 3000)
 
 app.get('/', (req, res) => {
     const { turno, estado } = req.query;
@@ -46,5 +46,4 @@ app.get('/', (req, res) => {
     }
 });
 
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(app.get('port'), () => console.log(`Example app listening at http://localhost:${app.get('port')}`));
